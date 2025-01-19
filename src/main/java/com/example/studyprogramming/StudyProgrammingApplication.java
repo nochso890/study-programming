6b187efbd3beb75ebbe78d2b9b1cc0175c11125e
nochso <nochso890@gmail.com>
2025-01-19 21:20:07 +0900

    init commit

---
+++ src/main/java/com/example/studyprogramming/StudyProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyProgrammingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyprogramming/StudyProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyProgrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
